0eed35bb47145a96adcae7d3e1f32d2e220aa69e
mullin28 <Mullin-C14@ulster.ac.uk>
2024-12-02 23:52:35 +0000

    Add files via upload

---
+++ Admin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Admin : User
+{
+	public DateTime LastLogin { get; set; }
+
+	private static List<Product> products = new List<Product>();
+
+	public void AddProduct(string name, decimal price, int stock)
+	{
+		Product newProduct = new Product
+		{
+			ProductID = products.count + 1,
+			Name = name,
+			Price = price,
+			Stock = stock
+		};
+
+		products.Add(newProduct);
+		Console.WriteLine("Product has been added successfully!");
+	}
+
+	public void RemoveProduct(int productId)
+	{
+		var product = products(p => p.ProductID == productId);
+		if (product != null)
+		{
+			product.Remove(product);
+			Console.WriteLine("Product has been Removed");
+
+		}
+		else
+
+			Console.WriteLine("Product has not been found");
+		}
+	}
+	
+	public void UpdateStock(int productId, int newStock)
+	{
+	var product = products.Find(product => product.ProductID == productId);
+	if (product != null)
+	{
+		product.Stock = newStock;
+		Console.WriteLine("Product stock updated");
+	}
+	else
+	{
+		Console.WriteLine("Product was not updated");
+		}
+	}		
+	
+	public void AddItem(int orderId, int productId, int quantity)
+	{
+		Console.WriteLine($"Item (ProductID: {productId}) added to order (OrderID: {orderId} with quantity: {quantity}");
+	}
+
+	public void RemoveItem(int orderId, int productId)
+	{
+	Console.WriteLine($"Item (ProductID: {productId}) removed from order (OrderID: {orderId}");
+	}
+}
---
+++ User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class User
+{
+	public User()
+	{
+	public int UserID { get; set; }
+	public string UserName { get; set; }
+	public string Password { get; set; }
+	public string Email { get; set; }
+	public string PhoneNumber { get; set; }
+	public string AddressStreet { get; set; }
+	public string AddressCity { get; set; }
+
+	public string void Register(string UserName, string Password, string Email, string PhoneNumber, string AddressStreet, string AddressCity)
+	{
+		User newUser = new User();
+		{
+			UserID = users.Count + 1,
+			UserName = UserName,
+			Password = Password,
+			Email = Email,
+			PhoneNumber = PhoneNumber,
+			AddressStreet = AddressStreet,
+			AddressCity = AddressCity,
+		};
+
+		users.Add(newUser);
+		Console.WriteLine("Registration Authorised");
+	}
+
+	public static User Login(string UserName, string Password)
+	{
+		foreach (var in users)
+		{
+			if (user.UserName == UserName && User.Password == Password)
+			{
+				Console.WriteLine("Login Successful");
+				return user;
+			}
+		}
+		Console.WriteLine("Invalid Username or Password, Please Try Again")
+		return null;
+	}
+
+	public static void ForgotPassword(string email)
+	{
+		foreach (var users in users)
+		{
+			if (users.Email == email)
+			{
+				Console.WriteLine($"Password reset link is being sent to {email}");
+				return;
+			}
+		}
+		Console.WriteLine("Your Email is not in the database");
+	}
+}
