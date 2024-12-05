d59fe26a4a21b54aa6aaf8c0123aa51d0fd70f88
mullin28 <Mullin-C14@ulster.ac.uk>
2024-12-05 19:19:01 +0000

    Update User.cs

--- User.cs
+++ User.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 public class User
 {
+    //Properties added
     public int UserID { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
